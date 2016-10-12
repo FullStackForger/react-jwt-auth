1707525d83eae5f1cf81325876d4ddc5a7d394a6
FullStackForger <fullstackforger@gmail.com>
2016-10-12 01:56:02 +0100

    Google component

---
+++ src/components/google.js
@@ -0,0 +1,41 @@
+import React, { Component, PropTypes } from 'react'
+import OAuth2 from './oauth2'
+
+const defaultProps = {
+	clientId: null,
+	name: 'google',
+	label: 'Sign in with Google',
+	tokenEndpoint: '/auth/google',
+	oauthProvider: 'google',
+	oauthEndpoint: 'https://accounts.google.com/o/oauth2/auth',
+	redirectUri: window.location.origin,
+	requiredUrlParams: ['scope'],
+	optionalUrlParams: ['display', 'state'],
+	scope: ['profile', 'email'],
+	scopePrefix: 'openid',
+	scopeDelimiter: ' ',
+	display: 'popup',
+	oauthType: '2.0',
+	popupOptions: { width: 452, height: 533 },
+	style: {
+		color: '#fff',
+		backgroundColor: '#dd4b39',
+		border: '1px solid #d54331',
+		padding: '5px 15px'
+	},
+	state: () => encodeURIComponent(Math.random().toString(36).substr(2))
+}
+
+export default class Google extends Component {
+
+	render () {
+		return (
+			<OAuth2 {...this.props}>
+				{this.props.children || null}
+			</OAuth2>
+		)
+	}
+}
+
+Google.defaultProps = defaultProps
+Google.propTypes = OAuth2.propTypes
