aab70a515d46a02b586e72a26da66762d3e66914
FullStackForger <fullstackforger@gmail.com>
2016-10-12 01:56:17 +0100

    main index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
+import { isAuthenticated, getToken, setToken, getAuthHeader } from './shared'
+import { login, logout, signup, refreshToken } from './local'
+import { getProfile, updateProfile } from './user'
+
+import Facebook from './components/facebook'
+export { Facebook }
+import Google from './components/google'
+export { Google }
+
+import config from './internals/config'
+
+import Auth from './components/auth'
+export {Auth}
+
 export default {
-  foo: 'bar'
+	init: config.init,
+	Auth,
+	Facebook,
+	isAuthenticated,
+	login,
+	logout,
+	signup,
+	getToken,
+	setToken,
+	refreshToken,
+	getAuthHeader,
+	getProfile,
+	updateProfile
 }
