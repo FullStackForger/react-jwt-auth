740c2dc8f91266e86ecee16e6274a07cfc318712
FullStackForger <fullstackforger@gmail.com>
2016-10-22 08:24:54 +0100

    client/server check helper methods

--- src/internals/utils.js
+++ src/internals/utils.js
@@ -41,6 +41,14 @@ export const isDefined = (value) => {
 	return value !== undefined && value !== null
 }
 
+export const isClient = () => {
+	return typeof window !== 'undefined' && typeof window.document !== 'undefined'
+}
+
+export const isServer = () => {
+	return !isClient()
+}
+
 export const parseQueryString = (str) => {
 	let obj = {};
 	let key;
