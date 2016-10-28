25ce93718fd40d0bec578a051e876ce0653b1bf0
FullStackForger <fullstackforger@gmail.com>
2016-10-28 14:24:46 +0100

    tob@2.0.3 (polyfil for tests)

--- internals/test.setup.js
+++ internals/test.setup.js
@@ -1,5 +1,8 @@
 import jsdom from 'jsdom'
+import atob from 'atob'
 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>')
 global.window = document.defaultView
 global.navigator = { userAgent: 'node.js' }
+
+global.atob = atob
